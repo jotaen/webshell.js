6a9ad3c8ef6603afae6525524b6145dca2f8e12f
Jan Heuermann <jan@jotaen.net>
2016-07-28 21:15:00 +0200

    Implement minimal nodejs repl

---
+++ src/engines/commandline.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const initialState = require('../initialState')
+const createBuffer = require('../../test/buffer')
+const createStore = require('redux').createStore
+const reducers = require('../reducers/index')
+
+const cd = require('../commands/cd')
+const mkdir = require('../commands/mkdir')
+const pwd = require('../commands/pwd')
+
+const stdin = process.stdin
+stdin.resume()
+stdin.setEncoding('utf8')
+
+const store = createStore(reducers, initialState(reducers))
+
+const splitInput = (line) => {
+  const args = line.split(' ')
+  return {
+    command: args.shift(),
+    data: args.join(' ')
+  }
+}
+
+const prompt = () => {
+  const location = store.getState().currentLocation.join('/')
+  const ps1 = '/' + location + '$ '
+  process.stdout.write(ps1)
+}
+
+prompt()
+stdin.on('data', (line) => {
+  const sanitizedLine = line.replace(/(\r\n|\n|\r)/gm, '')
+  const input = splitInput(sanitizedLine)
+  const buffer = createBuffer()
+  if (input.command === 'pwd') pwd(input.data, buffer, store)
+  if (input.command === 'cd') cd(input.data, buffer, store)
+  if (input.command === 'mkdir') mkdir(input.data, buffer, store)
+  const output = buffer.get()
+  const newline = output === '' ? '' : '\n'
+  process.stdout.write(output + newline)
+  prompt()
+})
