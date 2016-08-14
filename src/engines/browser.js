3a62ac4d70b40b96ba80bf580f82f5b9aae10779
Jan Heuermann <jan@jotaen.net>
2016-08-14 11:03:31 +0200

    First breakthrough (HTML repl/demo works)

--- src/engines/browser.js
+++ src/engines/browser.js
@@ -1,40 +1,21 @@
 'use strict'
 
-const defaultState = require('../defaultState')
-const createBuffer = require('../buffer/textBuffer.js')
-const createStore = require('redux').createStore
-const reducers = require('../reducers/index')
-const commands = require('../commands/index')
+const createRepl = require('./repl.js')
 
 module.exports = (elementId) => {
   const inputElement = document.getElementById('webshell-current')
-  const store = createStore(reducers, defaultState(reducers))
-  const webshell = {}
-  let nextCommand
+  const repl = createRepl()
 
-  const dispatch = (input) => {
-    return input+': command not found'
-    const buffer = createBuffer()
-    if (typeof nextCommand === 'function') {
-      nextCommand = nextCommand(input.arg, buffer, store)
-    } else if (typeof commands[input.command] === 'function') {
-      nextCommand = commands[input.command](input.arg, buffer, store)
-    } else if (input.command !== '') {
-      buffer.print(input.command + ': command not found')
+  return {
+    process: (event) => {
+      if (event.keyCode !== 13) return;
+      const input = inputElement.innerHTML;
+      inputElement.insertAdjacentHTML('beforebegin', '<div class="input">'+input+'</div>');
+      inputElement.innerHTML = ''
+      const response = repl.execute(input)
+      inputElement.insertAdjacentHTML('beforebegin', '<div class="response">'+response+'</div>');
+      inputElement.insertAdjacentHTML('beforebegin', '<div class="prompt">jotaen@/$</div>');
+      return false;
     }
-    return buffer.get()
   }
-
-  webshell.process = (event) => {
-    if (event.keyCode !== 13) return;
-    const input = inputElement.innerHTML;
-    inputElement.insertAdjacentHTML('beforebegin', '<div class="input">'+input+'</div>');
-    inputElement.innerHTML = ''
-    const response = dispatch(input)
-    inputElement.insertAdjacentHTML('beforebegin', '<div class="response">'+response+'</div>');
-    inputElement.insertAdjacentHTML('beforebegin', '<div class="prompt">jotaen@/$</div>');
-    return false;
-  }
-
-  return webshell
 }
---
+++ src/engines/repl.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const defaultState = require('../defaultState')
+const createBuffer = require('../buffer/textBuffer.js')
+const createStore = require('redux').createStore
+const reducers = require('../reducers/index')
+const commands = require('../commands/index')
+
+const splitStatement = (line) => {
+  const parts = line.split(' ')
+  return {
+    command: parts.shift(),
+    input: parts.join(' ')
+  }
+}
+
+module.exports = () => {
+  const store = createStore(reducers, defaultState(reducers))
+  let nextCommand
+  return {
+    execute: (line) => {
+      const buffer = createBuffer()
+      const statement = splitStatement(line)
+      if (typeof nextCommand === 'function') {
+        nextCommand = nextCommand(statement.input, buffer, store)
+      } else if (typeof commands[statement.command] === 'function') {
+        nextCommand = commands[statement.command](statement.input, buffer, store)
+      } else if (statement.command !== '') {
+        buffer.print(statement.command + ': command not found')
+      }
+      return buffer.get()
+    }
+  }
+}
