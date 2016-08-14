2f6753a17ccc3179fd454a16c1ad92181bdeaf7f
Jan Heuermann <jan@jotaen.net>
2016-08-14 11:14:43 +0200

    Use BEM style for CSS

--- src/engines/browser.js
+++ src/engines/browser.js
@@ -3,21 +3,23 @@
 const createRepl = require('./repl.js')
 
 module.exports = (elementId) => {
-  const inputElement = document.getElementById('webshell-current')
+  const webshellElement = document.getElementById(elementId)
+  webshellElement.innerHTML = '<div class="webshell__input" id="' + elementId + '-current-input" contentEditable="true" onkeypress="return webshell.process(event)"></div>'
+  const inputElement = document.getElementById(elementId + '-current-input')
   const repl = createRepl()
 
-  inputElement.insertAdjacentHTML('beforebegin', '<div class="prompt">' + repl.prompt() + '</div>')
+  inputElement.insertAdjacentHTML('beforebegin', '<div class="webshell__prompt">' + repl.prompt() + '</div>')
 
   return {
     process: (event) => {
       if (event.keyCode !== 13) return
       const input = inputElement.innerHTML
-      inputElement.insertAdjacentHTML('beforebegin', '<div class="input">' + input + '</div>')
+      inputElement.insertAdjacentHTML('beforebegin', '<div class="webshell__input">' + input + '</div>')
       inputElement.innerHTML = ''
       const response = repl.execute(input)
-      inputElement.insertAdjacentHTML('beforebegin', '<div class="response">' + response + '</div>')
+      inputElement.insertAdjacentHTML('beforebegin', '<div class="webshell__response">' + response + '</div>')
       const prompt = repl.prompt()
-      inputElement.insertAdjacentHTML('beforebegin', '<div class="prompt">' + prompt + '</div>')
+      inputElement.insertAdjacentHTML('beforebegin', '<div class="webshell__prompt">' + prompt + '</div>')
       return false
     }
   }
