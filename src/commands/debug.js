50a372c50bafb9718e94cdebf512f029a35762e8
Jan Heuermann <jan@jotaen.net>
2016-08-17 13:01:25 +0200

    Fix syntax erro

--- src/commands/debug.js
+++ src/commands/debug.js
@@ -2,7 +2,7 @@
 
 module.exports = (input, terminal, state) => {
   if (input === 'state') {
-    const state = JSON.stringify(state, null, 2)
-    terminal.print(state)
+    const serializedState = JSON.stringify(state, null, 2)
+    terminal.print(serializedState)
   }
 }
