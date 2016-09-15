d936a001882d11d8c0442c1d318d100984b8efef
Jan Heuermann <jan@jotaen.net>
2016-09-15 18:28:46 +0200

    Enhance test

--- test/src/core/parse.test.js
+++ test/src/core/parse.test.js
@@ -5,8 +5,39 @@ const parse = require('../../../src/core/parse')
 
 describe('#parse', () => {
   it('generate a list of commands', () => {
-    const input = 'rm -rf test && echo "Hello \\"World\\" & other planets" | cat Hello\\ World.txt >> text.txt'
+    const input = 'rm -rf test && echo "Hello \\"World\\" & other planets" | cat Hello\\ World.txt >> text.txt & ls . > ls.txt'
     const result = parse(input)
-    assert(Array.isArray(result))
+    const expect = [
+      {
+        command: 'rm',
+        args: ['-rf', 'test'],
+        wantsInput: false,
+        stopOnFailure: true
+      }, {
+        command: 'echo',
+        args: ['Hello "World" & other planets'],
+        wantsInput: false,
+        stopOnFailure: true
+      }, { command: 'cat',
+        args: ['Hello World.txt'],
+        wantsInput: true,
+        stopOnFailure: true
+      }, { command: 'put',
+        args: ['--amend', 'text.txt'],
+        wantsInput: true,
+        stopOnFailure: true
+      }, {
+        command: 'ls',
+        args: ['.'],
+        wantsInput: false,
+        stopOnFailure: false
+      }, {
+        command: 'put',
+        args: ['--overwrite', 'ls.txt'],
+        wantsInput: true,
+        stopOnFailure: true
+      }
+    ]
+    assert.deepEqual(result, expect)
   })
 })
